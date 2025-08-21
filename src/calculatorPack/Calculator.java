986fbe4192fe5f5e923d29e697b96dcb419502ac
GiftedGiftB <asekhamegift@gmail.com>
2025-08-22 00:51:27 +0100

    Added more test to create the calculaor App

--- src/calculatorPack/Calculator.java
+++ src/calculatorPack/Calculator.java
@@ -2,8 +2,15 @@ package calculatorPack;
 
 public class Calculator {
 
-    public int add(int a, int b) {
+    public static int add(int a, int b) {
         return a+b;
     }
 
+    public static int subtract(int a, int b) {
+        return a-b;
+    }
+
+    public static int multipply(int a, int b) {
+        return a*b;
+    }
 }
--- test/calculatorTest/CalculatorTest.java
+++ test/calculatorTest/CalculatorTest.java
@@ -8,16 +8,27 @@ import static org.junit.Assert.assertEquals;
 public class CalculatorTest {
 
 //    @Test
-//    public void testThatCalculatorCanAdd(){
-//        int result = Calculator.add(3, 7);
-//        assertEquals(10, result);
+//    public void testThatCalculatorCanAdd_2(){
+//        Calculator calculator = new Calculator();
+//        calculator.add(10, 20);
+//        assertEquals(30, calculator.add(10, 20));
 //    }
 
     @Test
-    public void testThatCalculatorCanAdd_2(){
-        Calculator calculator = new Calculator();
-        calculator.add(10, 20);
-        assertEquals(30, calculator.add(10, 20));
+    public void testThatCalculatorCanAdd(){
+        int result = Calculator.add(3, 7);
+        assertEquals(10, result);
+    }
+
+    @Test
+    public void testThatCalculatorCanSubtract(){
+        int result = Calculator.subtract(10,2);
+        assertEquals(8, result);
+    }
+    @Test
+    public void testThatCalculatorCanMultiply(){
+        int result = Calculator.multipply(10,2);
+        assertEquals(20, result);
     }
 
 }
