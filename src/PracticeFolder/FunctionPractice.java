e83d40f1220e759e18426a1490e1d0c84f755fda
GiftedGiftB <asekhamegift@gmail.com>
2025-08-24 00:46:18 +0100

    Added more test to create the calculator App

---
+++ src/PracticeFolder/FunctionPractice.java
@@ -0,0 +1,13 @@
+package PracticeFolder;
+
+public class FunctionPractice {
+    public static void main(String[] args) {
+
+        taskTen();
+    }
+    public static void taskTen(){
+        for(char count = 'a'; count <= 'z'; count++){
+            System.out.print(count + " ");
+        }
+    }
+}
