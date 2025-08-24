af027b325053bfd6fe9a29b13b7f1d38ee3afcab
GiftedGiftB <asekhamegift@gmail.com>
2025-08-24 01:06:01 +0100

    tas

--- src/PracticeFolder/FunctionPractice.java
+++ src/PracticeFolder/FunctionPractice.java
@@ -3,11 +3,25 @@ package PracticeFolder;
 public class FunctionPractice {
     public static void main(String[] args) {
 
-        taskOne();
+        taskOne();System.out.println();
+        taskTwo();System.out.println();
+        taskThree();System.out.println();
     }
     public static void taskOne(){
         for(char count = 'a'; count <= 'z'; count++){
             System.out.print(count + " ");
+        }
+    }
+    public static void taskTwo(){
+        for(int count = 0; count <= 100; count++){
+           if(count % 2 == 1){
+                System.out.print(count + " ");
+           }
+        }
+    }
+    public static void taskThree(){
+        for(int count = 1; count <= 20; count++){
+            System.out.print(count * count + " ");
         }
     }
 }
