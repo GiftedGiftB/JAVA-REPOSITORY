e8eb713ba1fc7b0ac35d2a30dcc66b8cc364e962
GiftedGiftB <asekhamegift@gmail.com>
2025-09-06 23:06:06 +0100

    App that teaches mulltiplication

---
+++ src/PracticeFolder/chapter6/MultiplicationPractice.java
@@ -0,0 +1,34 @@
+package PracticeFolder.chapter6;
+import java.security.SecureRandom;
+import java.util.Scanner;
+
+public class MultiplicationPractice {
+    private static final SecureRandom = new SecureRandom();
+    private static final Scanner = new Scanner(System.in);
+
+    public static void main(String[] args){
+            System.out.print("Welcome to the Multiplication Practice Program");
+            askQuestion();
+    }
+
+    private static void askQuestion() {
+        int num1 = random.nextInt(9) + 1;
+        int num2 = random.nextInt(9) + 1;
+
+        int result = num1 * num2;
+        int userInput;
+    }
+    System.out.print("what is %d times %d? ", num1,num2);
+    userInput = Scanner.nextInt();
+
+    if(userInput == result){
+        System.out.println("Correct.");
+        askQuestion();
+    }
+    else{
+        System.out.println("Incorrect.");
+    }
+
+
+}
+
