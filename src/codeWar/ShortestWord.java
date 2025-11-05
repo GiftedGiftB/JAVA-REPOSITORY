e322a74d8e2fabfe669cbbefaed4ba1a3ae8e996
GiftedGiftB <asekhamegift@gmail.com>
2025-11-05 01:27:35 +0100

    This code returns the shortest word in a sentence

--- src/codeWar/ShortestWord.java
+++ src/codeWar/ShortestWord.java
@@ -1,4 +1,20 @@
 package codeWar;
 
 public class ShortestWord {
+    public static void main(String[] args) {
+        String sentence = "I love java what do you love";
+        String result = shortestWordInSentence(sentence);
+        System.out.println("The shortest word is: " + result);
+    }
+    public static String shortestWordInSentence(String sentence) {
+        String[] words = sentence.split(" ");
+        String shortestWord = words[0];
+        for (int count = 0; count < words.length; count++){
+            String tempWord = words[count];
+            if (tempWord.length() < shortestWord.length()){
+                shortestWord = tempWord;
+            }
+        }
+        return shortestWord;
+    }
 }
