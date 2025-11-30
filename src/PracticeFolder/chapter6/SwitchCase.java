e4d6a56289880d4dc6e175c08272afe028fec2d2
GiftedGiftB <asekhamegift@gmail.com>
2025-11-30 18:50:52 +0100

    switch statement program that print the day of the week.

--- src/PracticeFolder/chapter6/SwitchCase.java
+++ src/PracticeFolder/chapter6/SwitchCase.java
@@ -2,20 +2,35 @@ package PracticeFolder.chapter6;
 
 public class SwitchCase {
     public static void main(String[] args) {
-    int day = 3;
+    int day = 4;
 
     switch (day) {
         case 1:
-            System.out.print("monday");
+            System.out.print("Monday");
             break;
 
         case 2:
-            System.out.print("tuesday");
+            System.out.print("Tuesday");
+            break;
+
+        case 3:
+            System.out.print("Wednesday");
+            break;
+
+        case 4:
+            System.out.print("Thursday");
+            break;
+
+        case 5:
+            System.out.print("Friday");
+            break;
+
+        case 6:
+            System.out.print("Saturday");
             break;
 
         default:
             System.out.print("Invalid");
         }
     }
-
 }
