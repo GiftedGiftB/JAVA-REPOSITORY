f119defaac3e3913951c81deecd169028faadab4
GiftedGiftB <asekhamegift@gmail.com>
2025-12-02 22:33:38 +0100

    A program that print 1 - 100 using while loop.

---
+++ src/PracticeFolder/Revise/Loops/OneToHundredWhileLoop.java
@@ -0,0 +1,14 @@
+package PracticeFolder.Revise.Loops;
+
+public class OneToHundredWhileLoop {
+    public static void main(String[] args){
+
+        int number = 1;
+
+        while (number <= 100) {
+
+            System.out.print(number + ",");
+                number++;
+        }
+    }
+}
