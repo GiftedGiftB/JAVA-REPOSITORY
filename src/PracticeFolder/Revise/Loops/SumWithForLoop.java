ed3b223f2a9181f073b08be5c2c8606fc2ce7843
GiftedGiftB <asekhamegift@gmail.com>
2025-12-02 22:34:52 +0100

    A program that sum 1 - 50 using for loop.

---
+++ src/PracticeFolder/Revise/Loops/SumWithForLoop.java
@@ -0,0 +1,11 @@
+package PracticeFolder.Revise.Loops;
+
+public class SumWithForLoop {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int count = 1; count <= 50; count++) {
+            sum += count;
+        }
+        System.out.print(sum);
+    }
+}
