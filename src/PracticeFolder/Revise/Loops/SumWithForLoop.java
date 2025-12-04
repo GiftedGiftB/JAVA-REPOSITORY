254f87acfff3ebbada92f03fa2c1acadc3a0ebf6
GiftedGiftB <asekhamegift@gmail.com>
2025-12-04 22:45:56 +0100

    A program that sum 1 - 50 using while loop.

--- src/PracticeFolder/Revise/Loops/SumWithForLoop.java
+++ src/PracticeFolder/Revise/Loops/SumWithForLoop.java
@@ -6,6 +6,6 @@ public class SumWithForLoop {
         for (int count = 1; count <= 50; count++) {
             sum += count;
         }
-        System.out.print(sum);
+        System.out.print("The sum of 1-50 is: " + sum);
     }
 }
