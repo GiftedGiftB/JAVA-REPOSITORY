7b6538b0eec48107061ff31a0be3c8e42684dd80
GiftedGiftB <asekhamegift@gmail.com>
2025-12-07 23:57:31 +0100

    A program for skips even number.

--- src/PracticeFolder/Revise/Loops/SkipEvenNumber.java
+++ src/PracticeFolder/Revise/Loops/SkipEvenNumber.java
@@ -3,7 +3,7 @@ package PracticeFolder.Revise.Loops;
 public class SkipEvenNumber {
     public static void main(String[] args) {
 
-        for(int count = 1; count <= 12; count++){
+        for(int count = 1; count <= 20; count++){
             if(count % 2 == 0){
                 continue;
             }
