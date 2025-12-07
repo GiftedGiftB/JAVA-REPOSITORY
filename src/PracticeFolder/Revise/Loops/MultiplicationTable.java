1a76b8c718031bd97e784bf68c48199cb3f60b39
GiftedGiftB <asekhamegift@gmail.com>
2025-12-07 20:53:30 +0100

    A program for multiplication table.

---
+++ src/PracticeFolder/Revise/Loops/MultiplicationTable.java
@@ -0,0 +1,12 @@
+package PracticeFolder.Revise.Loops;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+
+        System.out.println("        Multiplication Table");
+
+        for(int count = 1; count <=12; count++){
+                System.out.println("5 * " + count + " = " + count * 5);
+        }
+    }
+}
