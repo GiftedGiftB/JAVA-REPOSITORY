8dd53ae0924d2396fbe1c74742d970b6ded51e8f
GiftedGiftB <asekhamegift@gmail.com>
2025-12-08 22:44:36 +0100

    A program that adds two numbers using method.

--- src/PracticeFolder/Revise/methodsFunctions/AddTwoNumbers.java
+++ src/PracticeFolder/Revise/methodsFunctions/AddTwoNumbers.java
@@ -1,4 +1,14 @@
 package PracticeFolder.Revise.methodsFunctions;
 
 public class AddTwoNumbers {
+    public static void main(String[] args){
+
+        addTwoNumber(2, 2);
+    }
+
+    public static void addTwoNumber(int a, int b){
+
+        int sum = a + b;
+        System.out.println("The answer is: " + sum);
+    }
 }
