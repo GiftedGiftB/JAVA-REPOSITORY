e3e17926e04545c6fcf2591495941dee7d1b7b12
GiftedGiftB <asekhamegift@gmail.com>
2025-12-08 23:00:54 +0100

    A program that returns the sum of number.

---
+++ src/PracticeFolder/Revise/methodsFunctions/ReturnSum.java
@@ -0,0 +1,13 @@
+package PracticeFolder.Revise.methodsFunctions;
+
+public class ReturnSum {
+    public static void main(String[] args) {
+
+        int result = sum(2,4);
+        System.out.println("The answer is: " + result);
+
+    }
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+}
